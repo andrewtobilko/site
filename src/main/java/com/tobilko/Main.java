89b403cbc644e9962e44df8cb52392b7f10007e4
Andrew Tobilko <andrew.tobilko@gmail.com>
2015-09-28 02:47:58 +0300

    it was added 2 classes

---
+++ src/main/java/com/tobilko/Main.java
@@ -0,0 +1,7 @@
+package com.tobilko;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/tobilko/Test.java
@@ -0,0 +1,7 @@
+package com.tobilko;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
